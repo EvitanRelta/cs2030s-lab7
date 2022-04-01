2ea16224c2cfe8577f7c4a38712452160a228b73
Shaun Tan <shauntanzongzhi@gmail.com>
2022-04-01 13:10:24 +0800

    Refactor 'run_all_tests.sh'

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -2,25 +2,48 @@
 # chmod +x path/to/file.sh
 clear
 DIVIDER="\n$(seq -s= $(tput cols - 1) | tr -d '[:digit:]')\n"
+header() {
+  local header=$1
+  printf "$DIVIDER$header\n"
+}
 
-printf "$DIVIDER[Compiling 'cs2030s.fp']\n"
+# Compiling my java classes
+#header "[Compiling 'cs2030s.fp']"
+header "[Compiling 'cs2030s.fp']"
 javac -Xlint:rawtypes,unchecked ./cs2030s/fp/*.java
 
-printf "$DIVIDER[Compiling + Running 'Test1']\n"
-javac -Xlint:rawtypes,unchecked Test1.java; java Test1 | grep -v ok | grep -P "(^\s+[^\.]|failed)"
-printf "$DIVIDER[Compiling + Running 'Test2']\n"
-javac -Xlint:rawtypes,unchecked Test2.java; java Test2 | grep -v ok | grep -P "(^\s+[^\.]|failed)"
-printf "$DIVIDER[Compiling + Running 'Test3']\n"
-javac -Xlint:rawtypes,unchecked Test3.java; java Test3 | grep -v ok | grep -P "(^\s+[^\.]|failed)"
-printf "$DIVIDER[Compiling + Running 'Test4']\n"
-javac -Xlint:rawtypes,unchecked Test4.java; java Test4 | grep -v ok | grep -P "(^\s+[^\.]|failed)"
-printf "$DIVIDER[Compiling + Running 'Test5']\n"
-javac -Xlint:rawtypes,unchecked Test5.java; java Test5 | grep -v ok | grep -P "(^\s+[^\.]|failed)"
-printf "$DIVIDER[Compiling + Running 'Test6']\n"
-javac -Xlint:rawtypes,unchecked Test6.java; java Test6 | grep -v ok | grep -P "(^\s+[^\.]|failed)"
+
+# Compiling + running tests
+header "[Compiling + Running 'Test1']"
+javac -Xlint:rawtypes,unchecked Test1.java
+java Test1 | grep -v ok | grep -P "(^\s+[^\.]|failed)"
+
+header "[Compiling + Running 'Test2']"
+javac -Xlint:rawtypes,unchecked Test2.java
+java Test2 | grep -v ok | grep -P "(^\s+[^\.]|failed)"
+
+header "[Compiling + Running 'Test3']"
+javac -Xlint:rawtypes,unchecked Test3.java
+java Test3 | grep -v ok | grep -P "(^\s+[^\.]|failed)"
+
+header "[Compiling + Running 'Test4']"
+javac -Xlint:rawtypes,unchecked Test4.java
+java Test4 | grep -v ok | grep -P "(^\s+[^\.]|failed)"
+
+header "[Compiling + Running 'Test5']"
+javac -Xlint:rawtypes,unchecked Test5.java
+java Test5 | grep -v ok | grep -P "(^\s+[^\.]|failed)"
+
+header "[Compiling + Running 'Test6']"
+javac -Xlint:rawtypes,unchecked Test6.java
+java Test6 | grep -v ok | grep -P "(^\s+[^\.]|failed)"
+
 
 # Check style
-printf "$DIVIDER[Checking styles - 'cs2030s.fp.InfiniteList']\n"; java -jar ~cs2030s/bin/checkstyle.jar -c ~cs2030s/bin/cs2030_checks.xml cs2030s/fp/InfiniteList.java;
+header "[Checking styles - 'cs2030s.fp.InfiniteList']"
+java -jar ~cs2030s/bin/checkstyle.jar -c ~cs2030s/bin/cs2030_checks.xml cs2030s/fp/InfiniteList.java;
+
 
 # Test generating 'javadoc'
-printf "$DIVIDER[Generating javadoc - 'cs2030s.fp.InfiniteList']\n"; javadoc -quiet -private -d docs cs2030s/fp/InfiniteList.java
+header "[Generating javadoc - 'cs2030s.fp.InfiniteList']"
+javadoc -quiet -private -d docs cs2030s/fp/InfiniteList.java
