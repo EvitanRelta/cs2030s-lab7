9cbdd8123ed6740bd77bf8fea62f19c8d4ba1625
Shaun Tan <shauntanzongzhi@gmail.com>
2022-04-01 12:36:28 +0800

    Improve 'grep' command for 'run_all_tests.sh'
    But exception messages are moved to top of their respective sections,
    due to how 'grep' works.

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -7,17 +7,17 @@ printf "$DIVIDER[Compiling 'cs2030s.fp']\n"
 javac -Xlint:rawtypes,unchecked ./cs2030s/fp/*.java
 
 printf "$DIVIDER[Compiling + Running 'Test1']\n"
-javac -Xlint:rawtypes,unchecked Test1.java; java Test1 | grep failed
+javac -Xlint:rawtypes,unchecked Test1.java; java Test1 | grep -v ok | grep -P "(^\s+[^\.]|failed)"
 printf "$DIVIDER[Compiling + Running 'Test2']\n"
-javac -Xlint:rawtypes,unchecked Test2.java; java Test2 | grep failed
+javac -Xlint:rawtypes,unchecked Test2.java; java Test2 | grep -v ok | grep -P "(^\s+[^\.]|failed)"
 printf "$DIVIDER[Compiling + Running 'Test3']\n"
-javac -Xlint:rawtypes,unchecked Test3.java; java Test3 | grep failed
+javac -Xlint:rawtypes,unchecked Test3.java; java Test3 | grep -v ok | grep -P "(^\s+[^\.]|failed)"
 printf "$DIVIDER[Compiling + Running 'Test4']\n"
-javac -Xlint:rawtypes,unchecked Test4.java; java Test4 | grep failed
+javac -Xlint:rawtypes,unchecked Test4.java; java Test4 | grep -v ok | grep -P "(^\s+[^\.]|failed)"
 printf "$DIVIDER[Compiling + Running 'Test5']\n"
-javac -Xlint:rawtypes,unchecked Test5.java; java Test5 | grep failed
+javac -Xlint:rawtypes,unchecked Test5.java; java Test5 | grep -v ok | grep -P "(^\s+[^\.]|failed)"
 printf "$DIVIDER[Compiling + Running 'Test6']\n"
-javac -Xlint:rawtypes,unchecked Test6.java; java Test6 | grep failed
+javac -Xlint:rawtypes,unchecked Test6.java; java Test6 | grep -v ok | grep -P "(^\s+[^\.]|failed)"
 
 # Check style
 printf "$DIVIDER[Checking styles - 'cs2030s.fp.InfiniteList']\n"; java -jar ~cs2030s/bin/checkstyle.jar -c ~cs2030s/bin/cs2030_checks.xml cs2030s/fp/InfiniteList.java;
